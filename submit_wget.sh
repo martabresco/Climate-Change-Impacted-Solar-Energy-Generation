12e891d27f50531cea745b4adb87f140503d3c53
Marta Bresco <s233224@dtu.dk>
2025-03-03 19:02:19 +0100

    tas interp and sarah bias

--- submit_wget.sh
+++ submit_wget.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 #SBATCH --time=12:00:00
-#SBATCH --job-name=CNRM-CM6-1
+#SBATCH --job-name=UKESM1-0-LL
 #SBATCH --nodes=1
 #SBATCH --mail-user=s233224@dtu.dk
 #SBATCH --mail-type=END
 #SBATCH --exclusive=user
 #SBATCH --partition=rome,workq
 
-cd /groups/FutureWind/SFCRAD/CNRM-CM6-1/historical/r1i1p1f2
+cd /groups/FutureWind/SFCRAD/UKESM1-0-LL/historical/r1i1p1f2
 
-bash /groups/FutureWind/SFCRAD/CNRM-CM6-1/historical/r1i1p1f2/wget_script_2025-3-1_11-39-21.sh -H
+bash /groups/FutureWind/SFCRAD/UKESM1-0-LL/historical/r1i1p1f2/wget_script_2025-3-3_18-50-51.sh -H
