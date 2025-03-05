1f4b295ccd02260a76cb786fca0a278a3df782e7
Marta Bresco <s233224@dtu.dk>
2025-03-05 16:38:13 +0100

    kept trying sarah

--- submit_wget.sh
+++ submit_wget.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 #SBATCH --time=12:00:00
-#SBATCH --job-name=UKESM1-0-LL
+#SBATCH --job-name=HadGEM3-GC31-MM
 #SBATCH --nodes=1
 #SBATCH --mail-user=s233224@dtu.dk
 #SBATCH --mail-type=END
 #SBATCH --exclusive=user
 #SBATCH --partition=rome,workq
 
-cd /groups/FutureWind/SFCRAD/UKESM1-0-LL/historical/r1i1p1f2
+cd /groups/FutureWind/SFCRAD/HadGEM3-GC31-MM/historical/r1i1p1f3
 
-bash /groups/FutureWind/SFCRAD/UKESM1-0-LL/historical/r1i1p1f2/wget_script_2025-3-3_18-50-51.sh -H
+bash /groups/FutureWind/SFCRAD/HadGEM3-GC31-MM/historical/r1i1p1f3/wget_script_2025-3-5_14-9-56.sh -H
